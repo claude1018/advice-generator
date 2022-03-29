feb3772eac596dc7082e54b1c2a712dfabeb84a0
claude1018 <ejeraclaude@outlook.ph>
2022-03-29 11:10:57 +0000

    update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home({ id, advice }: SlipType) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const response = await fetch('https://api.adviceslip.com/advice');
   const advice: AdviceSlip = await response.json();
   const slip: SlipType = advice.slip;
